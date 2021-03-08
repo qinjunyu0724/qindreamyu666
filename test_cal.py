95558f79f7b4fe8732639edc7df5e7b70debfbd9
qinjunyu0724 <1831158470@qq.com>
2021-03-08 16:55:47 +0800

    xiugai

--- test_cal.py
+++ test_cal.py
@@ -4,10 +4,13 @@ def add(a,b):
     return a-b
 
 class Test_Cal():
-    def setup(self):
+    def setup_class(self):
         print("开始计算")
     @pytest.mark.parametrize("a,b",[(8,9),(6,3),(3,7)])
     def test_add(self,a,b):
         assert add(a,b)==a-b
+
+    def teardown_class(self):
+        print("计算结束")
 
 
